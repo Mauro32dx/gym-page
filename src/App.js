46563ab2678911154310041d66e31877701da9cb
Mauro32dx <mauromam32dx@gmail.com>
2021-01-08 03:56:28 -0300

    se establecio la estructura

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Principal from './Principal/Principal.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Probando COMMIT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Principal />
     </div>
   );
 }
---
+++ src/Principal/Principal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Principal() {
+    return (
+      <div>
+        BarraMenu
+        Presentacion
+        Home
+        Actividades
+        Horarios
+        Personal
+        Tarifa
+        Contacto
+        PieDePagina
+      </div>
+    );
+  }
+  
+  export default Principal;
